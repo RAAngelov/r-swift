8c414ff2f89e97398a43072e762ec979cd739d59
tau <tau@DESKTOP-Q4DDUAT>
2017-02-22 20:12:02 +0200

    ...

--- Homework Assignments/src/Homework_3/Task4f_LongestCommonSubstring.java
+++ Homework Assignments/src/Homework_3/Task4f_LongestCommonSubstring.java
@@ -1,8 +1,18 @@
 package Homework_3;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Task4f_LongestCommonSubstring {
+	public static char [] getSubstring(int ind,int L,char [] myString){
+		char [] str1 = new char [L];
+		for (int i = 0;i <= L - 1;i++){
+			str1[i] = myString[i + ind];
+		}
+
+		return str1;
+		
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -19,9 +29,31 @@ public class Task4f_LongestCommonSubstring {
 		}
 		char [] Str = str.toCharArray();
 		char [] Str1 = str1.toCharArray();
-		
-		
+		boolean flag = false;
+		for (int L = mlen;L >= 1;L--){
+			if(flag){
+				break;
+			}
+			for (int i = 0;i < mlen - L + 1 ;i++){
+				if(flag){
+					break;
+				}
+				for (int j = 0;j < Str1.length - L + 1;j++){
+					if(flag){
+						break;
+					}
+					if(Arrays.equals(getSubstring(j,L,Str1),getSubstring(i,L,Str))){
+	                    char [] ch = getSubstring(j,L,Str1);
+	                    for (int m = 0;m < L;m++){
+	                    	System.out.print(ch[m]);
+	                    }
+						flag = true;
+						break;
+					}
 
+				}
+			}
+		}
 	}
 
 }
