4724c61fee1c939eeca63b9996adef1fdc5b3c27
tau <tau@DESKTOP-Q4DDUAT>
2017-02-22 16:27:55 +0200

    ...

---
+++ Homework Assignments/src/Homework_3/Task4e_BracketMatching.java
@@ -0,0 +1,52 @@
+package Homework_3;
+
+import java.util.Scanner;
+
+public class Task4e_BracketMatching {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string :");
+		String str = sc.nextLine();
+		char [] Str = str.toCharArray();
+		int length = Str.length;
+		if (length % 2 != 0 || !((Str[0] == '(' && Str[length - 1] == ')') ||
+				(Str[0] == '[' && Str[length - 1] == ']') ||
+				(Str[0] == '{' && Str[length - 1] == '}'))){
+			System.out.println("false");
+		}
+		if (length == 2 && (Str[0] == '(' && Str[length - 1] == ')') ||
+				(Str[0] == '[' && Str[length - 1] == ']') ||
+				(Str[0] == '{' && Str[length - 1] == '}')){
+			System.out.println("true");	
+		}
+		boolean flag = true;
+		for (int K = 0;K <= length - 2;K += 2){
+			for (int i = 1;i + K < length - 2;i++){
+				if ((Str[i] == '(' && Str[i + K + 1] == ')') ||
+						(Str[i] == '[' && Str[i + K + 1] == ']') ||
+						(Str[i] == '{' && Str[i + K + 1] == '}')){
+					for (int j = i + 1;j <= i + K;j++){
+						if (Str[j] != '0'){
+							flag = false;
+						}
+					}
+					    if (flag){
+					        Str[i] = '0';
+					        Str[i + K + 1] = '0';
+					    }
+					}
+			
+				
+			}
+		}
+		if(flag){
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+         
+		
+	}
+
+}
