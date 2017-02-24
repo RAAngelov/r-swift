9a2d683d7fbee62d43282b02c1f52808c12538f3
tau <tau@DESKTOP-Q4DDUAT>
2017-02-24 23:28:37 +0200

    ...

--- Homework Assignments/Task1_Methods/Task1_Printing.java
+++ Homework Assignments/Task1_Methods/Task1_Printing.java
@@ -1,6 +1,5 @@
-
 public class Task1_Printing {
-	public static void Task1_Printing(String name,int age){
+	public static void Task1_Printing(String name, int age) {
 		System.out.println(name + " is " + age + " years old");
 	}
 
