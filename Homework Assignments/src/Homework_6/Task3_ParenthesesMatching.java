36fdbf10f68eb41985b96d48d08ce5cbb06c0e53
tau <tau@DESKTOP-Q4DDUAT>
2017-03-31 18:09:21 +0300

    ...

--- Homework Assignments/src/Homework_6/Task3_ParenthesesMatching.java
+++ Homework Assignments/src/Homework_6/Task3_ParenthesesMatching.java
@@ -1,20 +1,34 @@
 package Homework_6;
+
 import java.util.Stack;
 import java.util.Scanner;
 import java.util.*;
+
 public class Task3_ParenthesesMatching {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		Stack st = new Stack();
-		char [] str1 = str.toCharArray();
-		for (int i = 0;i < str1.length;i++){
-			if (lqva skoba){
-				
-			} else if (proverka){
-				
-			} 
+		char[] str1 = str.toCharArray();
+		for (int i = 0; i < str1.length; i++) {
+			if (str1[i] == '(' || str1[i] == '{' || str1[i] == '[') {
+				st.add(str1[i]);
+			} else if (str1[i] == ')' && ((char) st.peek() == '{' || (char) st.peek() == '[')
+					|| str1[i] == '}' && ((char) st.peek() == '(' || (char) st.peek() == '[')
+					|| str1[i] == ']' && ((char) st.peek() == '{' || (char) st.peek() == '(')) {
+
+				System.out.println("False");
+				break;
+
+			} else if ((str1[i] == ']' && (char) st.peek() == '[') || (str1[i] == ')' && (char) st.peek() == '(') ||
+
+			(str1[i] == '}' && (char) st.peek() == '{')) {
+				st.pop();
+			}
+		}
+		if (st.isEmpty()) {
+			System.out.println("True");
 		}
 
 	}
